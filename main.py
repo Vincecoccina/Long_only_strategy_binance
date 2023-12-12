e7594c1f93cb848ce620c856c549ff4e64b8630e
Vince <v.coccina@orange.fr>
2023-12-12 17:40:10 +0100

    Long only trader Class

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
 from binance.client import Client
+from binance import ThreadedWebsocketManager
 from dotenv import load_dotenv
+import time
 
 # Chargement des variables d'environnement pour la sécurité des clés API
 load_dotenv()
@@ -10,3 +12,51 @@ secret_key = os.getenv("SECRET_KEY_TEST")
 
 # Initialisation de l'API Binance avec les clés API
 client = Client(api_key=api_key, api_secret=secret_key, tld='com', testnet=True)
+
+df = pd.DataFrame(columns = ["Open", "High", "Low", "Close", "Volume", "Complete"])
+symbol = 'BTCUSDT'
+
+class LongOnlyTrader():
+    
+    def __init__(self, symbol, bar_length):
+        self.symbol = symbol
+        self.bar_length = bar_length
+        self.data = pd.DataFrame(columns = ["Open", "High", "Low", "Close", "Volume", "Complete"])
+        self.available_intervals = ["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]
+    
+    def start_trading(self):
+        
+        self.twm = ThreadedWebsocketManager()
+        self.twm.start()
+        
+        if self.bar_length in self.available_intervals:
+            self.twm.start_kline_socket(callback = self.stream_candles,
+                                        symbol = self.symbol, interval = self.bar_length)
+        # "else" to be added later in the course 
+    
+    def stream_candles(self, msg):
+        
+        # extract the required items from msg
+        event_time = pd.to_datetime(msg["E"], unit = "ms")
+        start_time = pd.to_datetime(msg["k"]["t"], unit = "ms")
+        first   = float(msg["k"]["o"])
+        high    = float(msg["k"]["h"])
+        low     = float(msg["k"]["l"])
+        close   = float(msg["k"]["c"])
+        volume  = float(msg["k"]["v"])
+        complete=       msg["k"]["x"]
+    
+        # print out
+        print("Time: {} | Price: {}".format(event_time, close))
+    
+        # feed df (add new bar / update latest bar)
+        self.data.loc[start_time] = [first, high, low, close, volume, complete]
+
+trader = LongOnlyTrader(symbol=symbol, bar_length="1m")
+trader.start_trading()
+run_time = 60 
+
+time.sleep(run_time)
+
+trader.twm.stop()
+print(trader.data)
